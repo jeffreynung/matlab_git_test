8b079c9ef6d855b8032af8403685a7a1d3b56b93
Mona Lisa <jeffrey.liu@terasoft.com.tw>
2025-09-19 09:27:49 +0800

    修正執行價格變數

--- blackScholes.m
+++ blackScholes.m
@@ -1,8 +1,8 @@
-function [putPrice, callPrice] = blackScholes(spotPrice, strikePrice, interestRate, timeToExpiration, volatility)
+function [putPrice, callPrice] = blackScholes(spotPrice, K, interestRate, timeToExpiration, volatility)
     % Calculate the Black-Scholes put and call option prices
-    d1 = (log(spotPrice / strikePrice) + (interestRate + 0.5 * volatility^2) * timeToExpiration) / (volatility * sqrt(timeToExpiration));
+    d1 = (log(spotPrice / K) + (interestRate + 0.5 * volatility^2) * timeToExpiration) / (volatility * sqrt(timeToExpiration));
     d2 = d1 - volatility * sqrt(timeToExpiration);
     
-    putPrice = strikePrice * exp(-interestRate * timeToExpiration) * normcdf(-d2) - spotPrice * normcdf(-d1);
-    callPrice = spotPrice * normcdf(d1) - strikePrice * exp(-interestRate * timeToExpiration) * normcdf(d2);
+    putPrice = K * exp(-interestRate * timeToExpiration) * normcdf(-d2) - spotPrice * normcdf(-d1);
+    callPrice = spotPrice * normcdf(d1) - K * exp(-interestRate * timeToExpiration) * normcdf(d2);
 end
